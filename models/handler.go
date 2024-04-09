b1038d89e1a1ebbb25dc9025e2d01908234aaf62
goeer <3480340468@qq.com>
2024-04-09 19:03:26 +0800

    增加了文件的上传功能，目前只测试了pdf的格式，还有对于token的测试，防止多次提交访问，能够有一定的预防作用

--- models/handler.go
+++ models/handler.go
@@ -1,11 +1,17 @@
 package models
 
 import (
+	"crypto/md5"
 	"fmt"
 	"html/template"
+	"io"
 	"log"
 	"net/http"
+	"os"
+	"regexp"
+	"strconv"
 	"strings"
+	"time"
 )
 
 func SayhelloName(w http.ResponseWriter, r *http.Request) {
@@ -25,12 +31,98 @@ func SayhelloName(w http.ResponseWriter, r *http.Request) {
 func Login(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("method:", r.Method) //获取请求的方法
 	if r.Method == "GET" {
+		timestamp := strconv.Itoa(time.Now().Nanosecond())
+		hashWr := md5.New()
+		hashWr.Write([]byte(timestamp))
+		token := fmt.Sprintf("%x", hashWr.Sum(nil))
+		//如果是请求login页的话就给他
 		t, _ := template.ParseFiles("views/login.gtpl")
-		// fmt.Println("这里有问题！！！")
+		log.Println(t.Execute(w, token))
+	} else {
+		//请求的是登录数据，那么执行登录的逻辑判断
+		r.ParseForm()
+		token := r.Form.Get("token")
+		if token != "" {
+			//验证token的合法性
+		} else {
+			//不存在token报错
+		}
+		flag := true
+		if m, _ := regexp.MatchString("^[a-zA-Z]+$", r.Form.Get("username")); !m {
+			flag = false
+		}
+		if m, _ := regexp.MatchString(`^([\w\.\_]{2,10})@(\w{1,})\.([a-z]{2,4})$`, r.Form.Get("email")); !m {
+			flag = false
+		}
+		if m, _ := regexp.MatchString(`^(?![0-9a-zA-Z]+$)(?![a-zA-Z!@#$%^&*]+$)(?![0-9!@#$%^&*]+$)[0-9A-Za-z!@#$%^&*]{8,16}$`, r.Form.Get("password")); !m {
+			flag = false
+		}
+		if !flag {
+			fmt.Fprintf(w, "error") //对于错误进行笼统的概括，留坑
+		} else {
+			t, _ := template.ParseFiles("views/submit.gtpl")
+			log.Println(t.Execute(w, nil))
+		}
+		fmt.Println("username length:", len(r.Form["username"][0]))
+		fmt.Println("username:", template.HTMLEscapeString(r.Form.Get("username"))) //输出到服务器端
+		fmt.Println("password:", template.HTMLEscapeString(r.Form.Get("password")))
+		template.HTMLEscape(w, []byte(r.Form.Get("username"))) //输出到客户端
+	}
+}
+func Submit(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method:", r.Method) //获取请求的方法
+	if r.Method == "GET" {
+		//如果是请求login页的话就给他
+		// t, _ := template.ParseFiles("views/submit.gtpl")
+		// log.Println(t.Execute(w, nil))
+		t, err := template.New("foo").Parse(`{{define "T"}}{{.}}{{end}}`)
+		err = t.ExecuteTemplate(w, "T", template.HTML("<h1>千里之行，从这里开始呀</h1>"))
+		if err != nil {
+			fmt.Fprintf(w, "error")
+		}
+
+	} else {
+		fmt.Fprintf(w, "该功能未开发...")
+	}
+}
+func Enroll(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method:", r.Method) //获取请求的方法
+	if r.Method == "GET" {
+		//如果是请求login页的话就给他
+		t, _ := template.ParseFiles("views/enroll.gtpl")
 		log.Println(t.Execute(w, nil))
 	} else {
 		//请求的是登录数据，那么执行登录的逻辑判断
+		r.ParseForm()
 		fmt.Println("username:", r.Form["username"])
 		fmt.Println("password:", r.Form["password"])
+	}
+}
+func Upload(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method:", r.Method) //获取请求的方法
+	if r.Method == "GET" {
+		crutime := time.Now().Unix()
+		h := md5.New()
+		io.WriteString(h, strconv.FormatInt(crutime, 10))
+		token := fmt.Sprintf("%x", h.Sum(nil))
+
+		t, _ := template.ParseFiles("upload.gtpl")
+		t.Execute(w, token)
+	} else {
+		r.ParseMultipartForm(32 << 20)//开一个内存空间出来
+		file, handler, err := r.FormFile("uploadfile")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer file.Close()
+		fmt.Fprintf(w, "%v", handler.Header)
+		f, err := os.OpenFile("./src/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666) // 此处假设当前目录下已存在test目录
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer f.Close()
+		io.Copy(f, file)
 	}
 }
