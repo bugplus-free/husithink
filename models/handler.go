97a20b1c6da2efc4057d9b87e46f47793b2b7306
goeer <3480340468@qq.com>
2024-04-09 16:09:25 +0800

    测试分支

--- models/handler.go
+++ models/handler.go
@@ -14,6 +14,7 @@ func SayhelloName(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(r.Form) //这些信息是输出到服务器端的打印信息
 	fmt.Println("path", r.URL.Path)
 	fmt.Println("scheme", r.URL.Scheme)
+
 	fmt.Println(r.Form["url_long"])
 	for k, v := range r.Form {
 		fmt.Println("key:", k)
